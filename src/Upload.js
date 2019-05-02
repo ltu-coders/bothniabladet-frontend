f79aac71501f0943a7ac68dfcbc56689846e6b9f
Sebastian Häyry <sebastian.hayry@live.se>
2019-05-02 21:03:44 +0200

    visuell uppladdning

--- src/Upload.js
+++ src/Upload.js
@@ -2,12 +2,29 @@ import React from 'react';
 
 /**
  * Sida för uppladdning av bilder
+ * Ska lägga till labels och inputs från användaren.
  */
 function UploadImage(props) {
 
-    return <div className="container">
-        <p>UploadImage är här!</p>
-    </div>
+    return (<div className="container">
+        <p></p>
+        <div class="row">
+        <div class="offset-md-3 col-md-6">
+        <div class="form-group files">
+        <label>Ladda upp bild</label>
+        <input type="file" name="file" />
+        
+        </div>
+        <div><button type="button" class="btn btn-success btn-block" onClick="#" >Ladda upp</button>
+        </div>
+        
+        </div>
+   
+        </div>
+        
+        
+    </div>)
+   
 }
 
 /**
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './landing-page/landing-page.css'
+import './Upload-page/upload.css'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
